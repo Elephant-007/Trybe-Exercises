2a4d874dfacd60009532ce9049e1ccc398cb7fe7
FoReVeR-GH <dream.high.dev@gmail.com>
2021-10-14 21:25:08 -0300

    atualizaçao

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
@@ -1,14 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let array = [];
 
-for (let index = 1; index <= numbers.length; index += 1) {
-  for (let secondIndex = index - 1; secondIndex < index; secondIndex += 1) {
-    if (numbers[index] !== undefined) {
-      array.push(numbers[secondIndex] * numbers[index]);
-    }
-    else {
-      array.push(numbers[secondIndex] * 2);
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index + 1] !== undefined) {
+    array.push(numbers[index] * numbers[index + 1]);
+  } else {
+    array.push(numbers[index] * 2);
   }
 }
 
