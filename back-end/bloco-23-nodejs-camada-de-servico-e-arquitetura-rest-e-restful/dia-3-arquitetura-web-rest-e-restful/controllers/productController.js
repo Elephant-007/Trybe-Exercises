c43223087b31e79189e3f93b8a52522c43ede3fb
FoReVeR-GH <dream.high.dev@gmail.com>
2022-05-05 14:23:19 -0300

    exercicios 23.3

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/controllers/productController.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const ProductModel = require('../models/productModel');
+
+const router = express.Router();
+
+router.get('/', async (_req, res) => {
+  const products = await ProductModel.getAll();
+
+  res.status(200).json(products);
+});
+
+router.get('/:id', async (req, res) => {
+  const product = await ProductModel.getById(req.params.id);
+
+  res.status(200).json(product);
+});
+
+router.post('/', async (req, res) => {
+  const { name, brand } = req.body;
+
+  const newProduct = await ProductModel.add(name, brand);
+
+  res.status(201).json(newProduct);
+});
+
+router.delete('/:id', async (req, res) => {
+  const products = await ProductModel.exclude(req.params.id);
+
+  res.status(204).json(products);
+});
+
+router.put('/:id', async (req, res) => {
+  const { name, brand } = req.body;
+
+  const products = await ProductModel.update(req.params.id, name, brand);
+
+  res.status(200).json(products);
+});
+
+module.exports = router;
